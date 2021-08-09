0c765369f7d94d7b0ea4abf1960c910c54f78df6
Artem Loshkarov <Troj94@gmail.com>
2021-08-09 20:30:33 +0300

    add library buttons

---
+++ src/js/transitToMain.js
@@ -0,0 +1,29 @@
+
+const homeButtonEl = document.querySelector('.home-button');
+const myLibraryButtonEl = document.querySelector('.my-library-button');
+
+const headerEl = document.querySelector('#header');
+
+const headerFormEl = document.querySelector('#header-form');
+
+homeButtonEl.addEventListener("click", transitToHomePage);
+
+function transitToHomePage() {
+    headerEl.classList.remove('header-my-library');
+    headerEl.classList.add('header-home');
+    homeButtonEl.classList.add('header__link--active');
+    myLibraryButtonEl.classList.remove('header__link--active');
+    headerFormEl.classList.add('header__form');
+    headerFormEl.classList.remove('header__form-disable');
+}
+
+myLibraryButtonEl.addEventListener("click", transitToMyLibraryPage);
+
+function transitToMyLibraryPage() {
+    headerEl.classList.remove('header-home');
+    headerEl.classList.add('header-my-library');
+    homeButtonEl.classList.remove('header__link--active');
+    myLibraryButtonEl.classList.add('header__link--active');
+    headerFormEl.classList.remove('header__form');
+    headerFormEl.classList.add('header__form-disable');
+}
