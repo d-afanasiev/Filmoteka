0cfaf7ecb8d1f284fb6c21a28912fe667a7f4835
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-08-09 20:42:52 +0300

    fix transitToMain.js

--- src/js/transitToMain.js
+++ src/js/transitToMain.js
@@ -1,4 +1,3 @@
-
 const homeButtonEl = document.querySelector('.home-button');
 
 const myLibraryButtonEl = document.querySelector('.my-library-button');
@@ -7,33 +6,25 @@ const headerEl = document.querySelector('#header');
 
 const headerFormEl = document.querySelector('#header-form');
 
-homeButtonEl.addEventListener("click", transitToHomePage);
+homeButtonEl.addEventListener('click', transitToHomePage);
 
 function transitToHomePage() {
-    headerEl.classList.remove('header-my-library');
-    headerEl.classList.add('header-home');
-    homeButtonEl.classList.add('header__link--active');
-    myLibraryButtonEl.classList.remove('header__link--active');
-    headerFormEl.classList.add('header__form');
-    headerFormEl.classList.remove('header__form-disable');
+  headerEl.classList.remove('header-my-library');
+  headerEl.classList.add('header-home');
+  homeButtonEl.classList.add('header__link--active');
+  myLibraryButtonEl.classList.remove('header__link--active');
+  headerFormEl.classList.add('header__form');
+  headerFormEl.classList.remove('header__form-disable');
 }
 
-myLibraryButtonEl.addEventListener("click", transitToMyLibraryPage);
+myLibraryButtonEl.addEventListener('click', transitToMyLibraryPage);
 
 function transitToMyLibraryPage() {
-    headerEl.classList.remove('header-home');
-    headerEl.classList.add('header-my-library');
-    homeButtonEl.classList.remove('header__link--active');
-    myLibraryButtonEl.classList.add('header__link--active');
-    headerFormEl.classList.remove('header__form');
-    headerFormEl.classList.add('header__form-disable');
-    console.log("Home-page open");
-}
-
-const myLibraryButtonEl = document.querySelector('.my-library-button');
-
-myLibraryButtonEl.addEventListener("click", transitToMyLibraryPage);
-
-function transitToMyLibraryPage() {
-    console.log("My-Library-page open");
+  headerEl.classList.remove('header-home');
+  headerEl.classList.add('header-my-library');
+  homeButtonEl.classList.remove('header__link--active');
+  myLibraryButtonEl.classList.add('header__link--active');
+  headerFormEl.classList.remove('header__form');
+  headerFormEl.classList.add('header__form-disable');
+  console.log('Home-page open');
 }
