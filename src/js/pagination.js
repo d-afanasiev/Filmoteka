e494f32fd1a254e331e5fdce2ac008e05bdc92c7
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-08-11 13:01:09 +0300

    tui pagination change visiblePages to 5

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,43 +1,44 @@
 'use strict';
 
-import  Pagination  from 'tui-pagination';
+import Pagination from 'tui-pagination';
 
 export const container = document.getElementById('tui-pagination-container');
 export const itemsPerPage = 20;
-export const setContainerHidden = (arg) => { container.hidden = arg; };
+export const setContainerHidden = arg => {
+  container.hidden = arg;
+};
 
 const optionsForPagination = {
+  // Total number of items
+  totalItems: 1000,
 
-      // Total number of items
-      totalItems: 1000,
+  // Items per page
+  itemsPerPage: itemsPerPage,
 
-      // Items per page
-      itemsPerPage: itemsPerPage,
+  // Visible pages
+  visiblePages: 5,
 
-      // Visible pages
-      visiblePages: 10,
+  // Current page
+  page: 1,
 
-      // Current page
-      page: 1,
+  // center aligned
+  centerAlign: false,
 
-      // center aligned
-      centerAlign: false,
+  // default classes
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
 
-      // default classes
-      firstItemClassName: 'tui-first-child',
-      lastItemClassName: 'tui-last-child',
-
-      // enable usage statistics
-      usageStatistics: false
+  // enable usage statistics
+  usageStatistics: false,
 };
 export const opt = optionsForPagination;
 
 export let myPagination;
 
 export function pagination() {
-     myPagination = new Pagination(container, opt);
-    return myPagination;
-};
+  myPagination = new Pagination(container, opt);
+  return myPagination;
+}
 
 // export const myPagination = new Pagination(container, optionsForPagination);
 
