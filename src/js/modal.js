824b599483d3e8d356d94cbe217bc04cf607cb83
Maksym Chyshko <76655891+Maks2090@users.noreply.github.com>
2021-08-11 11:57:24 +0300

    modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -11,25 +11,25 @@ const refs = {
 };
 
 
-//  function fetchMovieId (movieID){
-//     const url = `${BASE_URL}${movieID}?api_key=${API_KEY}&language=en-US&id&poster_path&title&vote_average&vote_count&popularity&original_title&genres`;
+ function fetchMovieId (movieID){
+    const url = `${BASE_URL}${movieID}?api_key=${API_KEY}&language=en-US`;
 
-//     const response =  axios.get(url)
+    const response =  axios.get(url)
 
-//     return response.data
-// } 
+    return response.data
+} 
 
 
-
+fetchMovieId(436969)
 
 // const cardMovie = document.querySelector('.modal__template');
 
 // cardMovie.insertAdjacentHTML( 'beforeend', markup(fetchMovieId))
 
 
-refs.movie.addEventListener('click', onOpenModalWindow)
+// refs.movie.addEventListener('click', onOpenModalWindow)
 
-function onOpenModalWindow (evt){
-    evt.preventDefault();
-    modalWindow.classList.add('is-open');
-}
+// function onOpenModalWindow (evt){
+//     evt.preventDefault();
+//     modalWindow.classList.add('is-open');
+// }
