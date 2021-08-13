35217c948479088f0ed3cf14815f54246c4e5716
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-08-13 12:59:04 +0300

    fix genreList.js v.2

--- src/js/genreList.js
+++ src/js/genreList.js
@@ -4,7 +4,7 @@ fetchListGenre().then(data => {
   localStorage.setItem('genres', JSON.stringify(data.genres));
 });
 
-let genres = JSON.parse(localStorage.getItem('genre'));
+let genres = JSON.parse(localStorage.getItem('genres'));
 const genreList = genres.reduce((previousValue, data) => {
   let genreAll = { [data.id]: data.name };
   return { ...previousValue, ...genreAll };
