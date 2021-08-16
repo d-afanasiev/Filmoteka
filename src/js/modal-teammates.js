5837b3ae6bfd37e74051e5a391dfce0bf979b490
stas-yushkov <stas.yushkov@gmail.com>
2021-08-16 23:41:33 +0300

    Capitalize names

--- src/js/modal-teammates.js
+++ src/js/modal-teammates.js
@@ -1,18 +1,23 @@
 import teammateTpl from '../templates/teammate.hbs';
 import teammatesData from './json/teammates.json';
-console.log('🚀 ~ teammatesData', teammatesData);
 
-console.log('🚀 ~  teammateTpl', teammateTpl);
-
-const teammatesHTML = teammateTpl(teammatesData);
-console.log('🚀 ~  teammatesHTML', teammatesHTML);
-
+const teammatesHTML = teammateTpl(
+  teammatesData.map(teammate => {
+    teammate.role = teammate.role.toUpperCase();
+    teammate.name = teammate.name
+      .split('-')
+      .map(word => `${word[0].toUpperCase()}${word.slice(1).toLowerCase()}`)
+      .join(' ');
+    return teammate;
+  }),
+);
 const footerTeamLink = document.querySelector('.footer__team-link');
 const lightboxRef = document.querySelector('.js-team-lightbox');
 const closeLightboxBtn = document.querySelector('button[data-action="close-lightbox"]');
 const teamList = document.querySelector('.js-team-list');
 
 teamList.insertAdjacentHTML('beforeend', teammatesHTML);
+console.log('🚀 ~  teammatesHTML', teammatesHTML);
 
 footerTeamLink.addEventListener('click', onFooterTeamLinkClick);
 
@@ -23,6 +28,7 @@ function onFooterTeamLinkClick(e) {
   lightboxRef.addEventListener('click', onlightboxRefClick);
   window.addEventListener('keydown', onEscBtnKeydown);
 
+  // https://css-tricks.com/prevent-page-scrolling-when-a-modal-is-open/
   const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
   const body = document.body;
   body.style.position = 'fixed';
