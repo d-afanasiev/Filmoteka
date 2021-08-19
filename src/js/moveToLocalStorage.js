b28b638605f111aa7d79a478aa4c2fe64c7a0c49
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-08-19 23:06:23 +0300

    fix moveToLocalStorage

--- src/js/moveToLocalStorage.js
+++ src/js/moveToLocalStorage.js
@@ -6,24 +6,23 @@ function addToLibaryWatch() {
   onButtonWatch.addEventListener('click', watch);
 }
 
-  function watch(e) {
-    let filmId = e.target.dataset.movieId;
-    fetchMovieId(filmId).then(response => {
-      let answer = response.data;
-      if (e.target.textContent === 'add to Watched') {
-        save('watched', answer);
-        refreshPage('watched');
-        e.target.textContent = 'remove from Watched';
-        onButtonWatch.classList.add('modal__button-active');
-      } else {
-        deleteObj('watched', answer.id);
-        e.target.textContent = 'add to Watched';
-        onButtonWatch.classList.remove('modal__button-active');
-        refreshPage('watched');
-      }
-    });
-  }
-
+function watch(e) {
+  let filmId = e.target.dataset.movieId;
+  fetchMovieId(filmId).then(response => {
+    let answer = response.data;
+    if (e.target.textContent === 'add to Watched') {
+      save('watched', answer);
+      refreshPage('watched');
+      e.target.textContent = 'remove from Watched';
+      onButtonWatch.classList.add('modal__button-active');
+    } else {
+      deleteObj('watched', answer.id);
+      e.target.textContent = 'add to Watched';
+      onButtonWatch.classList.remove('modal__button-active');
+      refreshPage('watched');
+    }
+  });
+}
 
 function addToLibaryQueue() {
   const onButtonQueue = document.querySelector('.modal__button-add-queue');
@@ -48,24 +47,23 @@ function watch(e) {
   });
 }
 
-  function queue(e) {
-    let filmId = e.target.dataset.movieId;
-  }
-    fetchMovieId(filmId).then(response => {
-      let answer = response.data;
-      if (e.target.textContent === 'add to Queue') {
-        save('queue', answer);
-        refreshPage('queue');
-        e.target.textContent = 'remove from Queue';
-        onButtonQueue.classList.add('modal__button-active');
-      } else {
-        deleteObj('queue', answer.id);
-        e.target.textContent = 'add to Queue';
-        onButtonQueue.classList.remove('modal__button-active');
-        refreshPage('queue');
-      }
-    });
-  }
+function queue(e) {
+  let filmId = e.target.dataset.movieId;
 
+  fetchMovieId(filmId).then(response => {
+    let answer = response.data;
+    if (e.target.textContent === 'add to Queue') {
+      save('queue', answer);
+      refreshPage('queue');
+      e.target.textContent = 'remove from Queue';
+      onButtonQueue.classList.add('modal__button-active');
+    } else {
+      deleteObj('queue', answer.id);
+      e.target.textContent = 'add to Queue';
+      onButtonQueue.classList.remove('modal__button-active');
+      refreshPage('queue');
+    }
+  });
+}
 
 export default { addToLibaryWatch, addToLibaryQueue };
