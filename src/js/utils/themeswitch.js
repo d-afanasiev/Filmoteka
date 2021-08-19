b377dd6f04caa8b812af15c0e473bb3098bb001b
AlexandrSeleznyov <vuavso16@gmail.om>
2021-08-19 21:24:50 +0300

    fixTheme

--- src/js/utils/themeswitch.js
+++ src/js/utils/themeswitch.js
@@ -11,26 +11,28 @@ modal = document.querySelector(".modal");
 function chekLocalstorage() {
   if (localStorage.getItem("cheked") === "true") {
    checkBox.checked = true;
-    document.body.classList.add("dark-theme");
-    footer.classList.add("dark-theme");
-    modal.classList.add("dark-theme");
+   addTheme(Theme.DARK);
      }
 }
-      chekLocalstorage();
+chekLocalstorage();
 
+function removeTheme(theme){
+  document.body.classList.remove(theme);
+  footer.classList.remove(theme);
+  modal.classList.remove(theme);
+}
 
+function addTheme(theme){
+  document.body.classList.add(theme);
+  footer.classList.add(theme);
+  modal.classList.add(theme);
+}
 
 function onCheked(event){
    
-  
-       
     if (checkBox.checked) {
-      document.body.classList.remove("light-theme");
-      footer.classList.remove("light-theme");
-      modal.classList.remove("light-theme");
-      document.body.classList.add("dark-theme");
-      footer.classList.add("dark-theme");
-      modal.classList.add("dark-theme");
+    removeTheme(Theme.LIGHT);
+    addTheme(Theme.DARK);
 
       localStorage.setItem("Theme", "DARK");
     localStorage.setItem("cheked", "true");
@@ -39,12 +41,8 @@ function onCheked(event){
       return;
     }
 
-    document.body.classList.remove("dark-theme");
-    footer.classList.remove("dark-theme");
-    modal.classList.remove("dark-theme");
-    document.body.classList.add("light-theme");
-    footer.classList.add("light-theme");
-    modal.classList.add("light-theme");
+    removeTheme(Theme.DARK);
+    addTheme(Theme.LIGHT);
     localStorage.setItem("Theme", "LIGHT");
     localStorage.setItem("cheked", "false");
    }
